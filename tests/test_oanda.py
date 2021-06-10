f8c18263b939660b249b318305ec079dfdaa0b16
Anthony Wynne <12174019+ant358@users.noreply.github.com>
2021-06-10 23:27:29 +0100

    edit to tests

--- tests/test_oanda.py
+++ tests/test_oanda.py
@@ -1,5 +1,5 @@
 
-from src.oandabase.oanda import Oanda, Order, Account, DataFeed
+from oandabase.oanda import Oanda, Order, Account, DataFeed
 
 oanda = Oanda()
 oanda_live = Oanda(practice=False)
@@ -8,10 +8,11 @@ order = Order()
 acc = Account()
 data = DataFeed()
 
+
 class TestOanda:
 
     def test_token(self):
-        message1 = "Does the token str exist" 
+        message1 = "Does the token str exist"
         message2 = "Is the token the expected length"
         x = oanda.token
         assert isinstance(x, str), message1
@@ -24,25 +25,33 @@ class TestOanda:
         assert len(x) >= 10
 
     def test_set_base_url(self):
-        if oanda.practice == True:
-            message = f"Practice account url not set correctly - {oanda.base_url}"
-            assert oanda.base_url == 'https://api-fxpractice.oanda.com', message
-        elif oanda_live.practice == False:
-            message = f"Live account url not set correctly - {oanda_live.base_url}"
-            assert oanda_live.base_url == 'https://stream-fxtrade.oanda.com', message
+        if oanda.practice:
+            message = ("Practice account url not set correctly"
+                       f" - {oanda.base_url}")
+            assert oanda.base_url == \
+                'https://api-fxpractice.oanda.com', message
+
+        elif not oanda_live.practice:
+            message = ("Live account url not set correctly"
+                       f" - {oanda_live.base_url}")
+            assert oanda_live.base_url ==\
+                'https://stream-fxtrade.oanda.com', message
 
     def test_default_pair(self):
         message = f"Default pair should be 'EUR_USD - It's {oanda.pair}!"
         assert oanda.pair == 'EUR_USD', message
 
     def test_pair_switching(self):
-        message = f"Pair should have changed to 'GBP_USD' - It's {oanda_gbp.pair}"
+        message = ("Pair should have changed to 'GBP_USD'"
+                   f" - It's {oanda_gbp.pair}")
         assert oanda_gbp.pair == 'GBP_USD', message
 
     def test_set_headers(self):
-        message = f"Authorizarion Header not set correctly, it does not contain the API token!"
+        message = ("Authorizarion Header not set correctly,"
+                   " it does not contain the API token!")
         assert oanda.token in oanda.headers['Authorization'], message
-    
+
+
 class TestAccount():
 
     def test_get_account(self):
@@ -53,29 +62,34 @@ class TestAccount():
         assert len(x) >= 2, message2
         account_id = x['account']['id']
         message3 = ("The account info returned is not for the "
-        f"correct account! - acc:{oanda.account} - returned{account_id}")
+                    f"correct account! - acc:{oanda.account}"
+                    " - returned{account_id}")
         assert oanda.account == account_id, message3
 
     def test_account_balance(self):
-        message = f"There is no money in the account!! - balance returned = {acc.balance}"
+        message = ("There is no money in the account!! "
+                   f"- balance returned = {acc.balance}")
         assert float(acc.balance) > 0, message
+
 
 class TestOrder():
     pass
 
+
 class TestDataFeed():
 
     def test_data0(self):
-        if data.backfill == True:
+        if data.backfill:
             data_length = len(data.data0)
-            message = f"We should have got 500 candle back - got {data_length} instead"
-            assert  data_length == 500, message
+            message = ("We should have got 500 candle back "
+                       f"- got {data_length} instead")
+            assert data_length == 500, message
 
-        message2 = f"We did not get a list back fro data0 as expected"
+        message2 = "We did not get a list back fro data0 as expected"
         assert isinstance(data.data0, list), message2
 
         message3 = ("We did not get the expected keys 'complete', 'volume', "
-        "'time', 'bid', 'ask' back in the data")
+                    "'time', 'bid', 'ask' back in the data")
         assert 'complete' in data.data0[0].keys(), message3
         assert 'volume' in data.data0[0].keys(), message3
         assert 'time' in data.data0[0].keys(), message3
@@ -89,4 +103,3 @@ class TestDataFeed():
         assert 'h' in bid, message
         assert 'l' in bid, message
         assert 'c' in bid, message
-        
